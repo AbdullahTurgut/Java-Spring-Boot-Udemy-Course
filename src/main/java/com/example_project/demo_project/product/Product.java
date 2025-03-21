6c4e946c471ce2ebce497ae39604908c09769d88
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-22 01:52:55 +0300

    Initial commit

---
+++ src/main/java/com/example_project/demo_project/product/Product.java
@@ -0,0 +1,9 @@
+package com.example_project.demo_project.product;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+}
