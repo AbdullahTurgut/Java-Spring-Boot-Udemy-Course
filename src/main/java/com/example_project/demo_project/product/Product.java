31ed73685ab7a8bf4b14e1fe04d4bd1a0cef01e0
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-22 01:59:18 +0300

    Örnek bir sınıfta lombok annotationlarının kullanımı

--- src/main/java/com/example_project/demo_project/product/Product.java
+++ src/main/java/com/example_project/demo_project/product/Product.java
@@ -1,5 +1,16 @@
 package com.example_project.demo_project.product;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// getter ve setterları Lombok dependency ile halledeceğiz.
+//@Getter ve @Setter yazmak yerine "@Data" tek başına yeterli oluyor.
+// constructor lar içinde annotation kullandık
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     private Long id;
