e164535e6c4a8de58033affcf1ab47e2c85ff8af
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-23 04:04:49 +0300

    Created a product jsx file for example of listing our product.

--- frontend/react-crash-course/src/App.jsx
+++ frontend/react-crash-course/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Product } from "./component/Product";
 
 function App() {
-  return <div className="container text-primary">Hello to our React Crash Course</div>;
+  return <div className="container text-primary">
+    Hello to our React Crash Course
+    <Product />
+  </div>;
 }
 
 export default App;
