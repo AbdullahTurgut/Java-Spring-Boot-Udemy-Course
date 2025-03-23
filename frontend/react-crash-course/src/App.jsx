7e358b932a68fa05dcfb7444983d5a6f837d822f
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-23 03:06:04 +0300

    The frontend side of the project

---
+++ frontend/react-crash-course/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return <div className="container text-primary">Hello to our React Crash Course</div>;
+}
+
+export default App;
